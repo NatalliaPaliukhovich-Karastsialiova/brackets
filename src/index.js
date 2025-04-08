7d56c27167c0f696ae4ca85a205c13e3c8f3fd44
Natallia Paliukhovich-Karastsialiova <n.paliukhovich98@gmail.com>
2025-04-08 20:13:22 +0500

    add: implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strStack = [];
+  let flag = true;
+  const flatConfig = bracketsConfig.flat();
+  let closedBrackets = [];
+  let openBrackets = [];
+
+  flatConfig.forEach((element, index) => {
+    if(index % 2 !== 0) closedBrackets.push(element)
+    else openBrackets.push(element);
+  });
+  [...str].forEach(element => {
+    if(!closedBrackets.includes(element) ||
+      (closedBrackets.includes(element) && openBrackets.includes(element) && strStack[strStack.length - 1] !== element)){
+      strStack.push(element);
+    }else{
+      let openBracketIndex = flatConfig.lastIndexOf(element);
+      const prevBracket = flatConfig[openBracketIndex - 1];
+      let lastSetElement = strStack[strStack.length - 1]
+      if(lastSetElement !== prevBracket) flag = false;
+      else strStack.pop();
+    }
+  });
+  if(strStack.length > 0) flag = false;
+  return flag;
 }
+
+
